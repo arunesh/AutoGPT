423066f6377dd01a55a5f2cd506c71826e75119f
Arunesh Mishra <aruneshm@gmail.com>
2025-11-18 17:08:25 -0800

    Initial Commit

---
+++ AutoGPT/AutoGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoGPTApp.swift
+//  AutoGPT
+//
+//  Created by Arun Mishra on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
